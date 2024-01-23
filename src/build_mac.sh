84ca074674c60ecc0f26f7718eb6bfebb84dec53
MichaelTimmers6 <97582970+MichaelTimmers6@users.noreply.github.com>
2024-01-23 17:38:51 +1000

    better build file

--- src/build_mac.sh
+++ src/build_mac.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
 echo compiling and linking...
 time clang++ *.cpp -o MacBuild -std=c++17 -l SDL2 2> errors
-open errors
 
-# ./MacBuild
+#check if errors is empty
+if [ -s errors ]; then
+    open errors
+else
+    echo "Success!"
+    ./MacBuild
+fi
